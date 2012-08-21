238cf9616a924ba1f8fbebde4dd03e8f7fbf0078
Selwin Ong <selwin.ong@gmail.com>
2012-08-21 22:30:49 +0700

    PyPI packaging now includes management commands.

--- post_office/__init__.py
+++ post_office/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (0, 1, 0)
+VERSION = (0, 1, 1)
 
 from .backends import EmailBackend
 from .models import PRIORITY
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-post_office',
-    version='0.1.0',
+    version='0.1.1',
     author='Selwin Ong',
     author_email='selwin.ong@gmail.com',
     packages=['post_office'],
