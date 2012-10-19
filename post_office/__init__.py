1ed6f1bf025bb287776ab448b6e230ca3065f536
Selwin Ong <selwin.ong@gmail.com>
2012-10-19 18:08:48 +0700

    Bump version to 0.1.5.

--- post_office/__init__.py
+++ post_office/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (0, 1, 4)
+VERSION = (0, 1, 5)
 
 from .backends import EmailBackend
 from .models import PRIORITY
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-post_office',
-    version='0.1.4',
+    version='0.1.5',
     author='Selwin Ong',
     author_email='selwin.ong@gmail.com',
     packages=['post_office'],
