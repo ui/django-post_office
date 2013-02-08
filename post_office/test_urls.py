f0ba4e47d833eba841b25ba65b0743ba3b11abe0
steven <steven@ui.co.id>
2013-02-08 09:40:29 +0700

    Now added tests for testing the admin view to make sure it return 200 response

---
+++ post_office/test_urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls), name='admin'),
+)
