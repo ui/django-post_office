e4c4ca81bf51afc0d296b7e7bac841e22832a1a9
steven <steven@ui.co.id>
2013-07-17 15:59:38 +0700

    Handle exceptions on the send_queued_mail command

--- post_office/management/commands/send_queued_mail.py
+++ post_office/management/commands/send_queued_mail.py
@@ -1,10 +1,12 @@
 import tempfile
+import sys
 from optparse import make_option
 
 from django.core.management.base import BaseCommand
 
 from ...lockfile import FileLock
 from ...mail import send_queued
+from ...logutils import setup_loghandlers
 
 
 class Command(BaseCommand):
@@ -14,6 +16,14 @@ class Command(BaseCommand):
                     help='Number of processes used to send emails', default=1),
     )
 
+    def __init__(self):
+        super(Command, self).__init__()
+        self._logger = setup_loghandlers()
+
     def handle(self, *args, **options):
         with FileLock(tempfile.gettempdir() + "/post_office", timeout=1):
-            send_queued(options['processes'])
+            try:
+                send_queued(options['processes'])
+            except Exception, e:
+                self._logger.error(e, exc_info=sys.exc_info(), extra={'status_code': 500})
+                raise
