bd3a955613d8f14d85d8a4543de55d3991a33cdd
Yuri Prezument <y@yprez.com>
2013-08-21 19:32:35 +0300

    Failing test case for string priority issue, ref #23

--- post_office/tests/models.py
+++ post_office/tests/models.py
@@ -238,3 +238,15 @@ class ModelTest(TestCase):
             'content': [u"Invalid filter: 'titl'"],
             'html_content': [u"Unclosed tags: endblock "]
         })
+    def test_string_priority(self):
+        """
+        Regression test for:
+        https://github.com/ui/django-post_office/issues/23
+        """
+        emails = send(['to1@example.com'], 'from@a.com', priority='low')
+
+        self.assertEquals(emails[0].priority, PRIORITY.low)
+
+    def test_string_priority_exception(self):
+        invalid_priority_send = lambda: send(['to1@example.com'], 'from@a.com', priority='hgh')
+        self.assertRaises(ValueError, invalid_priority_send)
