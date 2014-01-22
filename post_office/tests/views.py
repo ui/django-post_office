4e4c14a07ef68d80597e8f3b1f1af00cb27f9192
Selwin Ong <selwin.ong@gmail.com>
2014-01-22 09:15:13 +0700

    Added admin change page test.

--- post_office/tests/views.py
+++ post_office/tests/views.py
@@ -3,6 +3,9 @@ from django.core.urlresolvers import reverse
 from django.test.client import Client
 from django.test import TestCase
 
+from post_office import mail
+from post_office.models import Email
+
 
 admin_username = 'real_test_admin'
 admin_email = 'read@admin.com'
@@ -18,4 +21,11 @@ class AdminViewTest(TestCase):
     # Small test to make sure the admin interface is loaded
     def test_admin_interface(self):
         response = self.client.get(reverse('admin:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_change_page(self):
+        """Ensure that changing an email object in admin works."""
+        mail.send(recipients=['test@example.com'])
+        email = Email.objects.latest('id')
+        response = self.client.get(reverse('admin:post_office_email_change', args=[email.id]))
         self.assertEqual(response.status_code, 200)
