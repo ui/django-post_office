912dddacf61a6852cbce13e53797268c3bedeab5
Selwin Ong <selwin.ong@gmail.com>
2014-01-23 09:05:35 +0700

    Hardcoded jsonfield requirement in setup.py to try and clear Travis' cache.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     zip_safe=False,
     include_package_data=True,
     package_data={'': ['README.rst']},
-    install_requires=['django>=1.2', 'jsonfield'],
+    install_requires=['django>=1.2', 'jsonfield==0.9.2'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
