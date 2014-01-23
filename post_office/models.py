08c9036994010a7249a4ab2382e444cb54617a2d
Selwin Ong <selwin.ong@gmail.com>
2014-01-23 08:50:26 +0700

    Model's __unicode__ should return unicode strings.

--- post_office/models.py
+++ post_office/models.py
@@ -148,7 +148,7 @@ class Log(models.Model):
         ordering = ('-date',)
 
     def __unicode__(self):
-        return str(self.date)
+        return unicode(self.date)
 
 
 class EmailTemplate(models.Model):
@@ -171,7 +171,7 @@ class EmailTemplate(models.Model):
         ordering = ('name',)
 
     def __unicode__(self):
-        return str(self.name)
+        return self.name
 
     def save(self, *args, **kwargs):
         template = super(EmailTemplate, self).save(*args, **kwargs)
