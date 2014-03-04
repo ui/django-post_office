ea2111823ca42c7379320b62deb830cf45a56f8b
Selwin Ong <selwin.ong@gmail.com>
2014-03-04 17:34:16 +0700

    First stab at using a new locking mechanism.

--- post_office/lockfile.py
+++ post_office/lockfile.py
@@ -1,79 +1,111 @@
-# Taken from http://www.evanfosmark.com/2009/01/cross-platform-file-locking-support-in-python/
+"""
+A file lock implementation that tries to avoid platform specific
+issues. It is inspired by a whole bunch of different implementations
+listed below.
 
+ - https://bitbucket.org/jaraco/yg.lockfile/src/6c448dcbf6e5/yg/lockfile/__init__.py
+ - http://svn.zope.org/zc.lockfile/trunk/src/zc/lockfile/__init__.py?rev=121133&view=markup
+ - http://stackoverflow.com/questions/489861/locking-a-file-in-python
+ - http://www.evanfosmark.com/2009/01/cross-platform-file-locking-support-in-python/
+ - http://packages.python.org/lockfile/lockfile.html
+
+There are some tests below and a blog posting conceptually the
+problems I wanted to try and solve. The tests reflect these ideas.
+
+ - http://ionrock.wordpress.com/2012/06/28/file-locking-in-python/
+
+I'm not advocating using this package. But if you do happen to try it
+out and have suggestions please let me know.
+"""
 import os
 import time
-import errno
- 
-class FileLockException(Exception):
-    pass
- 
+
+
+class FileLocked(Exception): pass
+
+
 class FileLock(object):
-    """ A file locking mechanism that has context-manager support so 
-        you can use it in a with statement. This should be relatively cross
-        compatible as it doesn't rely on msvcrt or fcntl for the locking.
-    """
- 
-    def __init__(self, file_name, timeout=10, delay=.05):
-        """ Prepare the file locker. Specify the file to lock and optionally
-            the maximum timeout and the delay between each attempt to lock.
-        """
-        self.is_locked = False
-        self.lockfile = os.path.join(os.getcwd(), "%s.lock" % file_name)
-        self.file_name = file_name
+
+    def __init__(self, fname, timeout=None, force=False):
+        self.fname = fname
+        self.lockfname = '%s.lock' % self.fname
         self.timeout = timeout
-        self.delay = delay
- 
- 
+        self.force = force
+        self.fh = None
+
+    def valid_lock(self):
+        """
+        See if the lock exists and is left over from an old process.
+        """
+
+        if not os.path.exists(self.lockfname):
+            return False
+
+        my_pid = os.getpid()
+        lock_pid = int(open(self.lockfname).read())
+
+        # this is our process
+        if my_pid == lock_pid:
+            return True
+
+        # it is/was another process
+        # see if it is running
+        try:
+            os.kill(lock_pid, 0)
+        except OSError:
+            os.remove(self.lockfname)
+            return False
+
+        # it is running
+        return True
+
+    def is_locked(self, force=False):
+        # We aren't locked
+        if not self.valid_lock():
+            return False
+
+        # We are locked, but we want to force it without waiting
+        if not self.timeout:
+            if self.force:
+                self.release()
+                return False
+            else:
+                # We're not waiting or forcing the lock
+                raise FileLocked()
+
+        # Locked, but want to wait for an unlock
+        interval = .1
+        intervals = int(self.timeout / interval)
+
+        while intervals:
+            if self.valid_lock():
+                intervals -= 1
+                time.sleep(interval)
+                #print('stopping %s' % intervals)
+            else:
+                return True
+
+        # check one last time
+        if self.valid_lock():
+            if self.force:
+                self.release()
+            else:
+                # still locked :(
+                raise FileLocked()
+
     def acquire(self):
-        """ Acquire the lock, if possible. If the lock is in use, it check again
-            every `wait` seconds. It does this until it either gets the lock or
-            exceeds `timeout` number of seconds, in which case it throws 
-            an exception.
-        """
-        start_time = time.time()
-        while True:
-            try:
-                self.fd = os.open(self.lockfile, os.O_CREAT|os.O_EXCL|os.O_RDWR)
-                break;
-            except OSError as e:
-                if e.errno != errno.EEXIST:
-                    raise 
-                if (time.time() - start_time) >= self.timeout:
-                    raise FileLockException("Timeout occured.")
-                time.sleep(self.delay)
-        self.is_locked = True
- 
- 
+        self.fh = os.open(self.lockfname, os.O_CREAT|os.O_EXCL|os.O_RDWR)
+        os.write(self.fh, str(os.getpid()))
+
     def release(self):
-        """ Get rid of the lock by deleting the lockfile. 
-            When working in a `with` statement, this gets automatically 
-            called at the end.
-        """
-        if self.is_locked:
-            os.close(self.fd)
-            os.unlink(self.lockfile)
-            self.is_locked = False
- 
- 
+        if self.fh:
+            os.close(self.fh)
+        os.remove(self.lockfname)
+
     def __enter__(self):
-        """ Activated when used in the with statement. 
-            Should automatically acquire a lock to be used in the with block.
-        """
-        if not self.is_locked:
+        if not self.is_locked():
             self.acquire()
         return self
- 
- 
+
     def __exit__(self, type, value, traceback):
-        """ Activated at the end of the with statement.
-            It automatically releases the lock if it isn't locked.
-        """
-        if self.is_locked:
-            self.release()
- 
- 
-    def __del__(self):
-        """ Make sure that the FileLock instance doesn't leave a lockfile
-            lying around.
-        """
         self.release()
--- post_office/tests/__init__.py
+++ post_office/tests/__init__.py
@@ -1,5 +1,6 @@
 from .backends import BackendTest
 from .commands import CommandTest
+from .lockfile import LockTest
 from .mail import MailTest
 from .models import ModelTest
 from .utils import UtilsTest
