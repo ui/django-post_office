7126952b64e94a3831964ec741188b8123d8a851
Selwin Ong <selwin.ong@gmail.com>
2014-04-03 09:48:17 +0700

    Bump version to 0.8.1.

--- post_office/__init__.py
+++ post_office/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (0, 8, 0)
+VERSION = (0, 8, 1)
 
 from .backends import EmailBackend
 from .models import PRIORITY
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-post_office',
-    version='0.8.0',
+    version='0.8.1',
     author='Selwin Ong',
     author_email='selwin.ong@gmail.com',
     packages=['post_office'],
