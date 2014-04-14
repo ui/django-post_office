57503968e4531855bb7e8a21bf5223809337d744
Selwin Ong <selwin.ong@gmail.com>
2014-04-14 16:16:31 +0700

    Uncomment all tests.

--- post_office/tests/__init__.py
+++ post_office/tests/__init__.py
@@ -1,8 +1,8 @@
-# from .backends import BackendTest
-# from .commands import CommandTest
+from .backends import BackendTest
+from .commands import CommandTest
 from .lockfile import LockTest
-# from .mail import MailTest
-# from .models import ModelTest
-# from .utils import UtilsTest
-# from .cache import CacheTest
-# from .views import AdminViewTest
+from .mail import MailTest
+from .models import ModelTest
+from .utils import UtilsTest
+from .cache import CacheTest
+from .views import AdminViewTest
