387f4588b45db06b06a702cd6561be9130f18021
Selwin Ong <selwin.ong@gmail.com>
2014-04-16 09:59:10 +0700

    Improved logging around file locking in send_queued_mail.

--- post_office/management/commands/send_queued_mail.py
+++ post_office/management/commands/send_queued_mail.py
@@ -4,7 +4,7 @@ from optparse import make_option
 
 from django.core.management.base import BaseCommand
 
-from ...lockfile import FileLock
+from ...lockfile import FileLock, FileLocked
 from ...mail import send_queued
 from ...logutils import setup_loghandlers
 
@@ -18,14 +18,19 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option('-p', '--processes', type='int',
                     help='Number of processes used to send emails', default=1),
-        make_option('-l', '--lockfile', type='string',
-                    help='Absolute path of lockfile to acquire', default=default_lockfile),
+        make_option('-l', '--lockfile', type='string', default=default_lockfile,
+                    help='Absolute path of lockfile to acquire'),
     )
 
     def handle(self, *args, **options):
-        with FileLock(options['lockfile'], timeout=1):
-            try:
-                send_queued(options['processes'])
-            except Exception as e:
-                logger.error(e, exc_info=sys.exc_info(), extra={'status_code': 500})
-                raise
+        logger.info('Acquiring lock for sending queued emails at %s.lock' %
+                    options['lockfile'])
+        try:
+            with FileLock(options['lockfile']):
+                try:
+                    send_queued(options['processes'])
+                except Exception as e:
+                    logger.error(e, exc_info=sys.exc_info(), extra={'status_code': 500})
+                    raise
+        except FileLocked:
+            logger.info('Failed to acquire lock, terminating now.')
