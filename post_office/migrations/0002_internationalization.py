9f4d84b537d458c44a34197bb1be021beb5b37dc
Jacob Rief <jacob.rief@gmail.com>
2015-05-29 11:45:21 +0200

    Added migration for Django>=1.7

---
+++ post_office/migrations/0002_internationalization.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import post_office.fields
+import post_office.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('post_office', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='TranslatedEmailTemplate',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('language', models.CharField(default=b'en-us', help_text='Render template in alternative language', max_length=12, choices=[(b'en-us', b'English')])),
+                ('description', models.TextField(help_text='Description of this template.', blank=True)),
+                ('subject', models.CharField(blank=True, max_length=255, verbose_name='Subject', validators=[post_office.validators.validate_template_syntax])),
+                ('content', models.TextField(blank=True, verbose_name='Content', validators=[post_office.validators.validate_template_syntax])),
+                ('html_content', models.TextField(blank=True, verbose_name='HTML Content', validators=[post_office.validators.validate_template_syntax])),
+                ('default_template', models.ForeignKey(related_name='translated_template', to='post_office.EmailTemplate')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AlterUniqueTogether(
+            name='translatedemailtemplate',
+            unique_together=set([('language', 'default_template')]),
+        ),
+        migrations.AlterField(
+            model_name='email',
+            name='bcc',
+            field=post_office.fields.CommaSeparatedEmailField(verbose_name='Bcc', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='email',
+            name='cc',
+            field=post_office.fields.CommaSeparatedEmailField(verbose_name='Cc', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='email',
+            name='from_email',
+            field=models.CharField(max_length=254, verbose_name='Email From', validators=[post_office.validators.validate_email_with_name]),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='email',
+            name='html_message',
+            field=models.TextField(verbose_name='HTML Message', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='email',
+            name='message',
+            field=models.TextField(verbose_name='Message', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='email',
+            name='subject',
+            field=models.CharField(max_length=255, verbose_name='Subject', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='email',
+            name='to',
+            field=post_office.fields.CommaSeparatedEmailField(verbose_name='Email To', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='emailtemplate',
+            name='content',
+            field=models.TextField(blank=True, verbose_name='Content', validators=[post_office.validators.validate_template_syntax]),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='emailtemplate',
+            name='html_content',
+            field=models.TextField(blank=True, verbose_name='HTML content', validators=[post_office.validators.validate_template_syntax]),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='emailtemplate',
+            name='subject',
+            field=models.CharField(blank=True, max_length=255, verbose_name='Subject', validators=[post_office.validators.validate_template_syntax]),
+            preserve_default=True,
+        ),
+    ]
