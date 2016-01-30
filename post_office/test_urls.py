6a830973fa8f29278015d55819dcbd87f0472ac9
Yuri Prezument <y@yprez.com>
2016-01-30 10:51:26 +0200

    Fix Django 1.10 url patterns warning

--- post_office/test_urls.py
+++ post_office/test_urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls), name='admin'),
-)
+]
