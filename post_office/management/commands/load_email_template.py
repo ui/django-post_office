aa38142c1c6798455dd729d273d7868329e763b4
Yuri Prezument <y@yprez.com>
2016-03-04 19:34:13 +0200

    Basic implementation of load_email_template command

---
+++ post_office/management/commands/load_email_template.py
@@ -0,0 +1,36 @@
+from django.core.management.base import BaseCommand
+from django.template import loader
+
+from ...models import EmailTemplate
+from ...logutils import setup_loghandlers
+
+logger = setup_loghandlers()
+
+
+def _get_template(template_name):
+    """Find the template and return the raw template file content"""
+
+    template = loader.get_template(template_name)
+    filename = template.origin.name
+
+    with open(filename) as f:
+        content = f.read()
+
+    content = content.strip()
+    return content
+
+
+class Command(BaseCommand):
+    help = 'Load an EmailTemplate from Django template files'
+    args = 'template_name'
+
+    def handle(self, template_name, *args, **options):
+        template_data = {
+            'subject': _get_template(template_name + '/subject.txt'),
+            'content': _get_template(template_name + '/content.txt'),
+            'html_content': _get_template(template_name + '/content.html'),
+        }
+
+        EmailTemplate.objects.update_or_create(name=template_name,
+                                               defaults=template_data)
+        logger.info('Loaded template: %s', template_name)
