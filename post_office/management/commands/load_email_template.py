60ff94ab63a5fe31b82657406aabe6998b09e0bb
Yuri Prezument <y@yprez.com>
2016-03-26 17:15:55 +0300

    When loading templates - strip subject only

--- post_office/management/commands/load_email_template.py
+++ post_office/management/commands/load_email_template.py
@@ -12,7 +12,6 @@ def _get_template(template_name):
 
     template = loader.get_template(template_name)
     content = template.template.source
-    content = content.strip()
     return content
 
 
@@ -21,12 +20,13 @@ class Command(BaseCommand):
     args = 'template_name'
 
     def handle(self, template_name, *args, **options):
-        template_data = {
-            'subject': _get_template(template_name + '/subject.txt'),
-            'content': _get_template(template_name + '/content.txt'),
-            'html_content': _get_template(template_name + '/content.html'),
-        }
+        subject = _get_template(template_name + '/subject.txt').strip()
+        content = _get_template(template_name + '/content.txt')
+        html_content = _get_template(template_name + '/content.html')
 
-        EmailTemplate.objects.update_or_create(name=template_name,
-                                               defaults=template_data)
+        EmailTemplate.objects.update_or_create(name=template_name, defaults={
+            'subject': subject,
+            'content': content,
+            'html_content': html_content,
+        })
         logger.info('Loaded template: %s', template_name)
