87214b1e988f7aa00dd5575b5c9c8ef4240365d6
Yuri Prezument <y@yprez.com>
2016-03-29 15:48:03 +0300

    Load templates command - catch IOErrors and show message

--- post_office/management/commands/load_email_template.py
+++ post_office/management/commands/load_email_template.py
@@ -9,25 +9,32 @@ from ... import settings
 logger = setup_loghandlers()
 
 
-def _get_template(template_name, filename):
-    """Find the template and return the raw template file content"""
-
-    path = settings.get_template_dir()
-
-    with open(os.path.join(path, template_name, filename)) as f:
-        content = f.read()
-
-    return content
-
-
 class Command(BaseCommand):
     help = 'Load an EmailTemplate from Django template files'
     args = 'template_name'
 
     def handle(self, template_name, *args, **options):
-        subject = _get_template(template_name, 'subject.txt').strip()
-        content = _get_template(template_name, 'content.txt')
-        html_content = _get_template(template_name, 'content.html')
+        base_dir = settings.get_template_dir()
+
+        def _get_template(filename):
+            with open(os.path.join(base_dir, template_name, filename)) as f:
+                content = f.read()
+            return content
+
+        try:
+            subject = _get_template('subject.txt').strip()
+            content = _get_template('content.txt')
+            html_content = _get_template('content.html')
+        except IOError as e:
+            message = str(e)
+            message += (
+                '\n\nPlease make sure you defined the correct email '
+                'template directory structure.\n'
+                'You can change the base directory ("{}") by setting '
+                'POST_OFFICE["TEMPLATE_DIR"] in your settings file.'
+            ).format(base_dir)
+            self.stderr.write(message)
+            return
 
         fields = {
             'subject': subject,
