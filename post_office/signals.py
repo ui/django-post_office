d7fc52cd3ae4dc381028aae52640f55b393f0b16
Simon Philips <simon.philips@becharged.eu>
2016-05-18 13:28:47 +0200

    Fix: Python 2.6 does not support the {} syntax for sets.

--- post_office/signals.py
+++ post_office/signals.py
@@ -8,6 +8,6 @@ from .fields import CommaSeparatedEmailField
 
 @receiver(post_init, sender=Email)
 def on_email_post_init(sender, instance, **kwargs):
-    for field in {"to", "cc", "bcc"}:
+    for field in ("to", "cc", "bcc"):
         value = CommaSeparatedEmailField.parse_value(getattr(instance, field))
         setattr(instance, field, value)
