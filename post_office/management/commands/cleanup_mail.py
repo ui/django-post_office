1d8aed7af72cf86715d7fad1812bc6b2b43cc119
Douglas Anderson <hockeybuggy@gmail.com>
2016-08-11 15:35:36 -0400

    Remove the use of `options_list` from the cleanup_mail command.

--- post_office/management/commands/cleanup_mail.py
+++ post_office/management/commands/cleanup_mail.py
@@ -1,5 +1,4 @@
 import datetime
-from optparse import make_option
 
 from django.core.management.base import BaseCommand
 from django.utils.timezone import now
@@ -9,10 +8,13 @@ from ...models import Email
 
 class Command(BaseCommand):
     help = 'Place deferred messages back in the queue.'
-    option_list = BaseCommand.option_list + (
-        make_option('-d', '--days', type='int', default=90,
-            help="Cleanup mails older than this many days, defaults to 90."),
-    )
+
+    def add_arguments(self, parser):
+        parser.add_argument('-d', '--days',
+            type=int,
+            default=90,
+            help="Cleanup mails older than this many days, defaults to 90."
+            )
 
     def handle(self, verbosity, days, **options):
         # Delete mails and their related logs and queued created before X days
