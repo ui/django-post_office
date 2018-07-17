6092687c1472aa34c17e01dcc0f300c49d65fcc1
Steven <stvn.bu@gmail.com>
2018-07-17 15:25:07 +0700

    Handle attachments cleanup (#234)
    * Handle attachments cleanup
    
    * Rename variables

--- post_office/management/commands/cleanup_mail.py
+++ post_office/management/commands/cleanup_mail.py
@@ -3,7 +3,7 @@ import datetime
 from django.core.management.base import BaseCommand
 from django.utils.timezone import now
 
-from ...models import Email
+from ...models import Attachment, Email
 
 
 class Command(BaseCommand):
@@ -11,15 +11,25 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('-d', '--days',
-            type=int,
-            default=90,
-            help="Cleanup mails older than this many days, defaults to 90."
-            )
+                            type=int, default=90,
+                            help="Cleanup mails older than this many days, defaults to 90.")
 
-    def handle(self, verbosity, days, **options):
+        parser.add_argument('-da', '--delete-attachments', action='store_true',
+                            help="Delete orphaned attachments.")
+
+    def handle(self, verbosity, days, delete_attachments, **options):
         # Delete mails and their related logs and queued created before X days
 
         cutoff_date = now() - datetime.timedelta(days)
         count = Email.objects.filter(created__lt=cutoff_date).count()
         Email.objects.only('id').filter(created__lt=cutoff_date).delete()
         print("Deleted {0} mails created before {1} ".format(count, cutoff_date))
+
+        if delete_attachments:
+            attachments = Attachment.objects.filter(emails=None)
+            attachments_count = len(attachments)
+            for attachment in attachments:
+                # Delete the actual file
+                attachment.file.delete()
+            attachments.delete()
+            print("Deleted {0} attachments".format(attachments_count))
