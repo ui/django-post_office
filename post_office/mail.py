d59c8757cd78531da58315e6532056d08cbb8302
Jacob Rief <jacob.rief@gmail.com>
2020-05-31 17:43:43 +0200

    improve logic

--- post_office/mail.py
+++ post_office/mail.py
@@ -290,10 +290,9 @@ def _send_bulk(emails, uses_multiprocessing=True, log_level=None):
     emails_failed = Email.objects.filter(id__in=email_ids)
     for email in emails_failed:
         if email.number_of_retries is None:
-            email.number_of_retries = 1
-        else:
+            email.number_of_retries = 0
+        if email.number_of_retries < max_retries:
             email.number_of_retries += 1
-        if email.number_of_retries <= max_retries:
             email.status = STATUS.requeued
             email.scheduled_time = scheduled_time
             num_requeued += 1
