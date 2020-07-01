983cc0a9cb7f9e57fa1d05741faab8cc567684af
Jacob Rief <jacob.rief@gmail.com>
2020-07-01 23:45:02 +0200

    unit test for Message-ID

--- post_office/test_settings.py
+++ post_office/test_settings.py
@@ -37,6 +37,7 @@ POST_OFFICE = {
         'connection_tester': 'post_office.tests.test_mail.ConnectionTestingBackend',
     },
     'MAX_RETRIES': 2,
+    'MESSAGE_ID_RIGHT': 'post_office.org',
 }
 
 
