7da6c3238abd22b29230aa794a15b46fbe0b343f
Jacob Rief <jacob.rief@uibk.ac.at>
2020-11-01 07:39:35 +0100

    restrict Pyton start method to OSX

--- post_office/test_settings.py
+++ post_office/test_settings.py
@@ -1,7 +1,7 @@
 import os
 import platform
 
-if platform.system() != "Linux":
+if platform.system() in ["Darwin"]:
     from multiprocessing import set_start_method
     # required since Python-3.8. See #319
     set_start_method("fork")
