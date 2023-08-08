f5d4eaa7cf2e98fca10b27ecc36e980b53e16041
marsha97 <angelamdm97@gmail.com>
2023-08-08 13:16:07 +0700

    not batching attachament

--- post_office/utils.py
+++ post_office/utils.py
@@ -157,18 +157,13 @@ def cleanup_expired_mails(cutoff_date, delete_attachments=True, batch_size=1000)
         if deleted_data:
             total_deleted_emails += deleted_data['post_office.Email']
 
-    attachments_count = 0
     if delete_attachments:
-        while True:
-            attachments = Attachment.objects.filter(emails=None)[:batch_size]
-            if not attachments:
-                break
-            attachment_ids = set()
-            for attachment in attachments:
-                # Delete the actual file
-                attachment.file.delete()
-                attachment_ids.add(attachment.id)
-            deleted_count, _ = Attachment.objects.filter(id__in=attachment_ids).delete()
-            attachments_count += deleted_count
+        attachments = Attachment.objects.filter(emails=None)
+        for attachment in attachments:
+            # Delete the actual file
+            attachment.file.delete()
+        attachments_count, _ = attachments.delete()
+    else:
+        attachments_count = 0
 
     return total_deleted_emails, attachments_count
