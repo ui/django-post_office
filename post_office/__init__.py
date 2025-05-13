a5b8d739f4d11cb68f485161be5d205a0bca13b3
blag <blag@users.noreply.github.com>
2025-05-13 15:06:16 -0700

    Convert setup.py to pyproject.toml

--- post_office/__init__.py
+++ post_office/__init__.py
@@ -1,7 +1,4 @@
-from ast import literal_eval
-from os.path import dirname, join
-
-with open(join(dirname(__file__), 'version.txt')) as fh:
-    VERSION = literal_eval(fh.read())
-
 from .backends import EmailBackend
+from .version import VERSION as VERSION_STRING
+
+VERSION = VERSION_STRING.split('.')
