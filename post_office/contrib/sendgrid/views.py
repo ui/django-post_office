2d6a493cecb352c3709ca2497b66460313adb6d2
blag <blag@users.noreply.github.com>
2025-05-26 03:45:48 -0600

    Rewrite webhook as a specialized CBV

--- post_office/contrib/sendgrid/views.py
+++ post_office/contrib/sendgrid/views.py
@@ -1,6 +1,8 @@
 import json
 import logging
 from datetime import datetime
+from enum import Enum
+from typing import Any
 
 from pytz import utc
 
@@ -8,147 +10,183 @@ from django.db import transaction
 from django.db.models import Q
 from django.http import HttpRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
 from post_office.models import Email, Log as EmailLog, STATUS
+from post_office.webhook import Event, BaseWebhookHandler
 
-from sendgrid_backend.decorators import verify_sendgrid_webhook_signature
+from sendgrid_backend.decorators import check_sendgrid_signature
 
 
 logger = logging.getLogger(__name__)
 
 
-ALL_SENDGRID_EVENTS = {
-    'Delivery events': [
-        # Message has been received by Sendgrid and is ready to be delivered to the final recipient
-        'processed',
-        # Message has been dropped from Sendgrid's queue and will not be retried
-        'dropped',
-        # Message has been successfully delivered to the receiving server
-        'delivered',
-        # Receiving server temporarily rejected the message
-        'deferred',
-        # Receiving server could not or would not accept mail to this recipient permanently
-        # Bounces can be either "conversational" (eg: immediately after being sent), or asynchronous
-        # See https://www.twilio.com/docs/sendgrid/ui/sending-email/bounces#asynchronous-bounces
-        'bounce',
-        # Receiving server could not or would not accept the message temporarily
-        'blocked',
-    ],
-    'Engagement events': [
-        # Recipient has opened the HTML message
-        'open',
-        # Recipient clicked on a link within the message
-        'click',
-        # Recipient marked message as spam
-        'spamreport',
-        # Recipient clicked on the 'Opt Out of All Emails' link (available after clicking the message's
-        # subscription management link)
-        'unsubscribe',
-        # Recipient unsubscribed from a specific group either by clicking the link directly or updating their
-        # preferences
-        'group_unsubscribe',
-        # Recipient resubscribed to a specific group by updating their preferences
-        'group_resubscribe',
-        # Your account status changed because of issues related to compliance with SendGrid's terms of service
-        'compliance_suspend',
-    ],
+class DeliverabilityEvent(Enum):
+    PROCESSED = "processed"
+    DROPPED   = "dropped"
+    DELIVERED = "delivered"
+    DEFERRED  = "deferred"
+    BOUNCE    = "bounce"
+    BLOCKED   = "blocked"
+
+
+class EngagementEvent(Enum):
+    OPEN               = "open"
+    CLICK              = "click"
+    SPAMREPORT         = "spamreport"
+    UNSUBSCRIBE        = "unsubscribe"
+    GROUP_UNSUBSCRIBE  = "group_unsubscribe"
+    GROUP_RESUBSCRIBE  = "group_resubscribe"
+
+
+class AccountEvent(Enum):
+    COMPLIANCE_SUSPENDED = "compliance_suspend"
+
+
+SENDGRID_STATUS_TO_EVENT = {
+    DeliverabilityEvent.PROCESSED.value: Event.ACCEPTED,
+    DeliverabilityEvent.DELIVERED.value: Event.DELIVERED,
+    DeliverabilityEvent.DEFERRED.value:  Event.DEFERRED,
+    DeliverabilityEvent.BOUNCE.value:    Event.HARD_BOUNCE,
+    DeliverabilityEvent.BLOCKED.value:   Event.SOFT_BOUNCE,
+    DeliverabilityEvent.DROPPED.value:   Event.REJECTED,
+
+    EngagementEvent.OPEN.value:        Event.OPEN,
+    EngagementEvent.CLICK.value:       Event.CLICK,
+    EngagementEvent.SPAMREPORT.value:  Event.SPAM_COMPLAINT,
+    EngagementEvent.UNSUBSCRIBE.value: Event.UNSUBSCRIBED,
+    EngagementEvent.GROUP_UNSUBSCRIBE: Event.UNSUBSCRIBED,
+    EngagementEvent.GROUP_RESUBSCRIBE: Event.RESUBSCRIBED,
+
+    AccountEvent.COMPLIANCE_SUSPENDED: Event.SUSPENDED,
 }
 
 
-# We need to coalesce Sendgrid event names to internal statuses
-SENDGRID_STATUS_TO_EMAIL_STATUS = {
+EVENT_TO_EMAIL_STATUS = {
+    # Message has been received by Sendgrid and is ready to be delivered to the final recipient
+    Event.ACCEPTED.value: STATUS.queued,
     # Message has been successfully delivered to the receiving server
-    'delivered': STATUS.sent,
-    # Message has been dropped from Sendgrid's queue and will not be retried
-    'dropped': STATUS.failed,
+    Event.DELIVERED.value: STATUS.sent,
+    # Receiving server temporarily rejected the message
+    Event.DEFERRED.value: STATUS.requeued,
     # Receiving server could not or would not accept mail to this recipient permanently
     # Bounces can be either "conversational" (eg: immediately after being sent), or asynchronous
     # See https://www.twilio.com/docs/sendgrid/ui/sending-email/bounces#asynchronous-bounces
-    'bounce': STATUS.failed,
+    Event.HARD_BOUNCE.value: STATUS.failed,
     # Receiving server could not or would not accept the message temporarily
-    'blocked': STATUS.failed,
-    # Message has been received by Sendgrid and is ready to be delivered to the final recipient
-    'processed': STATUS.queued,
-    # Receiving server temporarily rejected the message
-    'deferred': STATUS.requeued,
+    Event.SOFT_BOUNCE.value: STATUS.failed,
+    # Message has been dropped from Sendgrid's queue and will not be retried
+    Event.REJECTED.value: STATUS.failed,
+    Event.OPEN.value: None,
+    Event.CLICK.value: None,
+    Event.SPAM_COMPLAINT.value: None,
+    Event.UNSUBSCRIBED.value: None,
+    Event.RESUBSCRIBED.value: None,
+    Event.COMPLIANCE_SUSPENDED: None,
 }
 
 
-# https://www.twilio.com/docs/sendgrid/for-developers/tracking-events/event
-# Post office Log objects have a restricted set of status options
-SENDGRID_STATUS_TO_EMAIL_LOG_STATUS = {
-    # Delivery events
-    # Message has been successfully delivered to the receiving server
-    'delivered': STATUS.sent,
+EVENT_TO_EMAIL_LOG_STATUS = {
     # Message has been received by Sendgrid and is ready to be delivered to the final recipient
-    'processed': STATUS.sent,  # Email status: queued
+    Event.ACCEPTED.value: STATUS.sent,
+    # Message has been successfully delivered to the receiving server
+    Event.DELIVERED.value: STATUS.sent,
     # Receiving server temporarily rejected the message
-    'deferred': STATUS.sent,  # Email status: requeued
-    # Message has been dropped from Sendgrid's queue and will not be retried
-    'dropped': STATUS.failed,
+    Event.DEFERRED.value: STATUS.sent,
+    # Receiving server could not or would not accept mail to this recipient permanently
     # Bounces can be either "conversational" (eg: immediately after being sent), or asynchronous
     # See https://www.twilio.com/docs/sendgrid/ui/sending-email/bounces#asynchronous-bounces
-    'bounce': STATUS.failed,
+    Event.HARD_BOUNCE.value: STATUS.failed,
     # Receiving server could not or would not accept the message temporarily
-    'blocked': STATUS.failed,
+    Event.SOFT_BOUNCE.value: STATUS.failed,
+    # Message has been dropped from Sendgrid's queue and will not be retried
+    Event.REJECTED.value: STATUS.failed,
+    Event.OPEN.value: None,
+    Event.CLICK.value: None,
+    Event.SPAM_COMPLAINT.value: None,
+    Event.UNSUBSCRIBED.value: None,
+    Event.RESUBSCRIBED.value: None,
+    Event.COMPLIANCE_SUSPENDED: None,
 }
 
 
-@csrf_exempt
-@require_POST
-@verify_sendgrid_webhook_signature
-def sendgrid_deliverability_webhook_handler(request: HttpRequest) -> HttpResponse:
-    try:
-        # Data is coming from JSON, where the root object isn't necessarily
-        # guaranteed to be a list.
-        # We reverse this because the list appears to be ordered reverse-chronologically
-        for msg_dict in reversed(json.loads(request.body)):
-            logger.debug(msg_dict)
+class SendgridWebhookHandler(BaseWebhookHandler):
+    def verify_webhook(self, request: HttpRequest, *args, **kwargs) -> bool:
+        return check_sendgrid_signature(request)
 
-            # If we can't pick out the exact email, don't try to do anything
-            # This can happen if we use django-post_office and generate Emails and Logs
-            # before we configure and use this webhook handler
-            if email := Email.objects.filter(Q(message_id=msg_dict.get('smtp-id', None))).first():
-                # Not all webhook events are email status changes
-                logger.debug(f"{SENDGRID_STATUS_TO_EMAIL_STATUS.get(msg_dict['event'], None) = }")
-                # The value for 'delivered' in SENDGRID_STATUS_TO_EMAIL_STATUS' can be 0, which evaluates to False, so
-                # we check explicitly for None
-                event_status = SENDGRID_STATUS_TO_EMAIL_STATUS.get(msg_dict['event'], None)
-                if event_status is not None:
-                    logger.debug(f'{Email.STATUS_CHOICES[event_status][1] = }')
+    @csrf_exempt
+    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        try:
+            # Data is coming from JSON, where the root object isn't necessarily
+            # guaranteed to be a list.
+            # We reverse this because the list appears to be ordered reverse-chronologically
+            for msg_dict in reversed(json.loads(request.body)):
+                logger.debug(msg_dict)
 
-                    # Save both the email status and the corresponding log object, or neither
-                    log_datetime = datetime.fromtimestamp(msg_dict.get('timestamp'), tz=utc)
-                    with transaction.atomic():
-                        logger.debug(f'{email.last_updated} < {log_datetime} -> {email.last_updated < log_datetime}')
-                        if email.last_updated < log_datetime:
-                            # The UNIX timestamps from Sendgrid only have a time resolution of 1 second.
-                            # Sendgrid is sometimes so fast that the time between being processed and delivered is less than 1 second.
-                            # Additionally, the events in the webhook are not sorted by time (or they might be sorted by reverse time?)
-                            # so on top of relying on timestamp ordering, we also rely on status to avoid overwriting sent status with
-                            # queued (from Sendgrid's "processed") status
-                            logger.debug(
-                                f'not (email.status ({Email.STATUS_CHOICES[email.status][1]}) == STATUS.sent ({Email.STATUS_CHOICES[STATUS.sent][1]}) and '
-                                f"msg_dict['event'] ({msg_dict['event']}) == 'processed') -> "
-                                f'{not (email.status == STATUS.sent and msg_dict["event"] == "processed")}'
-                            )
-                            if not (email.status == STATUS.sent and msg_dict['event'] == 'processed'):
-                                logger.debug(f'Updating email status to: {EmailLog.STATUS_CHOICES[event_status][1]}')
-                                # Avoid calling .save() because that triggers auto_now, which sets last_update to now()
-                                Email.objects.filter(pk=email.pk).update(last_updated=log_datetime, status=event_status)
+                # If we can't pick out the exact email, don't try to do anything
+                # This can happen if we use django-post_office and generate Emails and Logs
+                # before we configure and use this webhook handler
+                email = Email.objects.filter(Q(message_id=msg_dict.get('smtp-id', None))).first()
+                event = SENDGRID_STATUS_TO_EVENT.get(msg_dict['event'], None)
 
-                        EmailLog.objects.create(
-                            email=email,
-                            date=datetime.utcfromtimestamp(msg_dict.get('timestamp')),
-                            status=SENDGRID_STATUS_TO_EMAIL_LOG_STATUS[msg_dict['event']],
-                            message=json.dumps(msg_dict),
-                        )
-                        logger.debug('Done saving webhook email logs')
-    except Exception as e:
-        logger.error(e)
-        # Raise an exception here so Django returns a 500 and emails the admins
-        # that something is wrong
-        raise Exception(
-            'Error when processing Sendgrid Events webhook. The Sendgrid Events Webhook API may have changed.'
-        ) from e
-    return HttpResponse('ok')
+                if event is not None:
+                    self.process_event(request, event, email=email, data=msg_dict)
+                else:
+                    self.unrecognized_event(request, msg_dict['event'], email=email, data=msg_dict)
+
+        except Exception as e:
+            logger.error(e)
+            # Raise an exception here so Django returns a 500 and emails the admins
+            # that something is wrong
+            raise Exception(
+                'Error when processing Sendgrid Events webhook. The Sendgrid Events Webhook API may have changed.'
+            ) from e
+        return HttpResponse('ok')
+
+    def accepted(self, request: HttpRequest, *args, email: Email | None = None, **kwargs) -> None:
+        self.log_event(request, *args, event=Event.ACCEPTED, email=email, **kwargs)
+
+    def delivered(self, request: HttpRequest, *args, email: Email | None = None, **kwargs) -> None:
+        self.log_event(request, *args, event=Event.DELIVERED, email=email, **kwargs)
+
+    def deferred(self, request: HttpRequest, *args, email: Email | None = None, **kwargs) -> None:
+        self.log_event(request, *args, event=Event.DEFERRED, email=email, **kwargs)
+
+    def hard_bounce(self, request: HttpRequest, *args, email: Email | None = None, **kwargs) -> None:
+        self.log_event(request, *args, event=Event.HARD_BOUNCE, email=email, **kwargs)
+
+    def soft_bounce(self, request: HttpRequest, *args, email: Email | None = None, **kwargs) -> None:
+        self.log_event(request, *args, event=Event.SOFT_BOUNCE, email=email, **kwargs)
+
+    def rejected(self, request: HttpRequest, *args, email: Email | None = None, **kwargs) -> None:
+        self.log_event(request, *args, event=Event.REJECTED, email=email, **kwargs)
+
+    def log_event(self, request: HttpRequest, *args, event: Event, email: Email, data: dict[str, Any], **kwargs) -> None:
+        email_status = EVENT_TO_EMAIL_STATUS[event.value]
+        email_log_status = EVENT_TO_EMAIL_LOG_STATUS[event.value]
+
+        # Save both the email status and the corresponding log object, or neither
+        log_datetime = datetime.fromtimestamp(data['timestamp'], tz=utc)
+        with transaction.atomic():
+            logger.debug(f'{email.last_updated} < {log_datetime} -> {email.last_updated < log_datetime}')
+            if email.last_updated < log_datetime:
+                # The UNIX timestamps from Sendgrid only have a time resolution of 1 second.
+                # Sendgrid is sometimes so fast that the time between being processed and delivered is less than 1 second.
+                # Additionally, the events in the webhook are not sorted by time (or they might be sorted by reverse time?)
+                # so on top of relying on timestamp ordering, we also rely on status to avoid overwriting sent status with
+                # queued (from Sendgrid's "processed") status
+                logger.debug(
+                    f'not (email.status ({Email.STATUS_CHOICES[email_status][1]}) == STATUS.sent ({Email.STATUS_CHOICES[STATUS.sent][1]}) and '
+                    f"data['event'] ({data['event']}) == 'processed') -> "
+                    f'{not (email.status == STATUS.sent and data["event"] == "processed")}'
+                )
+                if not (email.status == STATUS.sent and data['event'] == 'processed'):
+                    logger.debug(f'Updating email status to: {email_status[1]}')
+                    # Avoid calling .save() because that triggers auto_now, which sets last_update to now()
+                    Email.objects.filter(pk=email.pk).update(last_updated=log_datetime, status=email_status)
+
+            EmailLog.objects.create(
+                email=email,
+                date=log_datetime,
+                status=email_log_status,
+                message=json.dumps(data),
+            )
+            logger.debug('Done saving webhook email logs')
---
+++ post_office/webhook.py
@@ -0,0 +1,99 @@
+"""
+A collection of utilities to help process webhook events from email services
+"""
+from abc import ABCMeta, abstractmethod
+from enum import Enum
+
+from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
+from django.views import View
+
+from .models import Email
+
+
+class Event(Enum):
+    # Deliverability
+    ACCEPTED       = "accepted"
+    DELIVERED      = "delivered"
+    DEFERRED       = "deferred"
+    HARD_BOUNCE    = "hard_bounce"
+    SOFT_BOUNCE    = "soft_bounced"
+    REJECTED       = "rejected"
+
+    # Engagement
+    OPEN           = "opened"
+    CLICK          = "clicked"
+
+    # Complaints & unsubscribes
+    SPAM_COMPLAINT = "spam_complaint"
+    UNSUBSCRIBED   = "unsubscribed"
+    RESUBSCRIBED   = "resubscribed"
+
+    # Account
+    SUSPENDED      = "account_suspended"
+
+
+
+class BaseWebhookHandler(View):
+    __metaclass__ = ABCMeta
+
+    def dispatch(self, request, *args, **kwargs) -> HttpResponse:
+        # The same method as View.dispatch, but we verify the webhook once
+        # we verify the HTTP method
+        if request.method.lower() in self.http_method_names:
+            if not self.verify_webhook(request, *args, **kwargs):
+                return HttpResponseNotFound()
+            handler = getattr(
+                self,
+                request.method.lower(),
+                self.http_method_not_allowed,
+            )
+        else:
+            handler = self.http_method_not_allowed
+        return handler(request, *args, **kwargs)
+
+    @abstractmethod
+    def verify_webhook(self, request: HttpRequest, *args, **kwargs) -> bool:
+        pass
+
+    def process_event(self, request: HttpRequest, event: Event, email: Email | None = None, *args, **kwargs) -> None:
+        handler = getattr(self, Event.ACCEPTED.value, self.unrecognized_event)
+        return handler(request, email, *args, **kwargs)
+
+    def unrecognized_event(self, request: HttpRequest, event: str, *args, email: Email | None = None, **kwargs) -> None:
+        pass
+
+    def accepted(self, request: HttpRequest, *args, email: Email | None = None, **kwargs) -> None:
+        pass
+
+    def delivered(self, request: HttpRequest, *args, email: Email | None = None, **kwargs) -> None:
+        pass
+
+    def deferred(self, request: HttpRequest, *args, email: Email | None = None, **kwargs) -> None:
+        pass
+
+    def hard_bounce(self, request: HttpRequest, *args, email: Email | None = None, **kwargs) -> None:
+        pass
+
+    def soft_bounce(self, request: HttpRequest, *args, email: Email | None = None, **kwargs) -> None:
+        pass
+
+    def rejected(self, request: HttpRequest, *args, email: Email | None = None, **kwargs) -> None:
+        pass
+
+    def opened(self, request: HttpRequest, *args, email: Email | None = None, **kwargs) -> None:
+        pass
+
+    def clicked(self, request: HttpRequest, *args, email: Email | None = None, **kwargs) -> None:
+        pass
+
+    def spam_complaint(self, request: HttpRequest, *args, email: Email | None = None, **kwargs) -> None:
+        pass
+
+    def unsubscribed(self, request: HttpRequest, *args, email: Email | None = None, **kwargs) -> None:
+        pass
+
+    def resubscribed(self, request: HttpRequest, *args, email: Email | None = None, **kwargs) -> None:
+        pass
+
+    def account_suspended(self, request: HttpRequest, *args, email: Email | None = None, **kwargs) -> None:
+        pass
