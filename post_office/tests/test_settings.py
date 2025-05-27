54d9a63378a4d4679e2fdb7afbb57b847cf1f8d1
blag <blag@users.noreply.github.com>
2025-05-27 16:18:23 -0600

    Add an option to set file storage

---
+++ post_office/tests/test_settings.py
@@ -0,0 +1,38 @@
+from django.test import TestCase, override_settings
+
+from ..settings import get_file_storage
+
+
+class TestFileStorageSettings(TestCase):
+    @override_settings(
+        STORAGES={
+            'default': {
+                'BACKEND': 'django.core.files.storage.FileSystemStorage',
+            },
+            'staticfiles': {
+                'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
+            },
+        },
+        POST_OFFICE={},
+    )
+    def test_default_file_storage(self):
+        self.assertEqual(get_file_storage().__class__.__name__, 'FileSystemStorage')
+
+    @override_settings(
+        STORAGES={
+            'default': {
+                'BACKEND': 'django.core.files.storage.FileSystemStorage',
+            },
+            'staticfiles': {
+                'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage',
+            },
+            'post_office': {
+                'BACKEND': 'django.core.files.storage.InMemoryStorage',
+            },
+        },
+        POST_OFFICE={
+            'FILE_STORAGE': 'post_office',
+        },
+    )
+    def test_configured_file_storage(self):
+        self.assertEqual(get_file_storage().__class__.__name__, 'InMemoryStorage')
