c74cc94cad1766063030dbc1b2ba0e603ac7ee72
Selwin Ong <selwin.ong@gmail.com>
2025-12-26 11:12:57 +0700

    Fixed weird import statements

--- post_office/tests/__init__.py
+++ post_office/tests/__init__.py
@@ -1,8 +1,8 @@
-from .test_backends import BackendTest as BackendTest
-from .test_cache import CacheTest as CacheTest
-from .test_commands import CommandTest as CommandTest
-from .test_lockfile import LockTest as LockTest
-from .test_mail import MailTest as MailTest
-from .test_models import ModelTest as ModelTest
-from .test_utils import UtilsTest as UtilsTest
-from .test_views import AdminViewTest as AdminViewTest
+from .test_backends import BackendTest
+from .test_cache import CacheTest
+from .test_commands import CommandTest
+from .test_lockfile import LockTest
+from .test_mail import MailTest
+from .test_models import ModelTest
+from .test_utils import UtilsTest
+from .test_views import AdminViewTest
